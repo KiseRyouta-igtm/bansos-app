85907491c602f432263d7f63c1a497ae5499db0c
achmadfardani <saya.dani337@gmail.com>
2026-04-28 18:44:56 +0700

    BE: implementasi login sederhana dengan session

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
 import os
 
@@ -6,6 +6,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "database.db")
 
 app = Flask(__name__)
+app.secret_key = "secret123"
 
 def get_db():
     conn = sqlite3.connect(DB_PATH)
@@ -27,6 +28,30 @@ def init_db():
 
     conn.commit()
     conn.close()
+
+def cek_login():
+    return 'login' in session
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == "admin" and password == "123":
+            session['login'] = True
+            return redirect('/')
+        else:
+            return "Login gagal!"
+
+    return render_template("login.html")
+
+@app.route('/')
+def index():
+    if not cek_login():
+        return redirect('/login')
+
+    return "Dashboard"
 
 if __name__ == '__main__':
     init_db()
